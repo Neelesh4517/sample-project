ff87c4b5e878f0ac0b5e91a02d12351a1239141d
Neelesh <neeleshgoud45@gmail.com>
2025-09-19 09:52:56 +0530

    debugg

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import ProductsPage from "./basicpage";
 // import ProductsPage from "./basicpage";
 
 const App: React.FC = () => {
-  return (
+  return (<>
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a simple React + TypeScript app powered by Vite.</p>
+    </div>
     <Router>
       <Routes>
         <Route path="/" element={<ProductsPage />} />
       </Routes>
     </Router>
+    </>
   );
 };
 
